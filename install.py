cd0bfcc0ec960bacba6eb3efe33f0b7662368ad6
glandesmates <84640701+glandesmates@users.noreply.github.com>
2021-09-04 15:41:53 -0500

    Create install.py

---
+++ install.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('sudo cp gscan /usr/bin/')
+os.system('sudo chmod +x /usr/bin/gscan')
+print("You can use gscan in any location with 'gscan' command. You can delete this directory."
