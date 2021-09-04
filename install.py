f5a2d5f3a2899ae89c43b20e0b186ce451471797
glandesmates <84640701+glandesmates@users.noreply.github.com>
2021-09-04 15:44:22 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,5 +1,10 @@
 import os
 
+if os.path.exists('/usr/bin/gscan'):
+  os.system('sudo rm /usr/bin/gscan')
+elif os.path.exists('/usr/bin/glap'):
+  os.system('sudo rm /usr/bin/glap')
+
 os.system('sudo cp gscan /usr/bin/')
 os.system('sudo chmod +x /usr/bin/gscan')
 print("You can use gscan in any location with 'gscan' command. You can delete this directory."
